2bf6d583a45f15280b240afd53af837a857e2995
araf821 <araf.ahmed200@gmail.com>
2025-04-04 03:43:30 -0400

    refactor: remove useless ScribeAIApp class

--- app/src/main/java/com/example/scribeai/ScribeAIApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.scribeai
-
-import android.app.Application
-import com.example.scribeai.data.AppDatabase
-
-class ScribeAIApp : Application() {
-    val database by lazy { AppDatabase.getDatabase(this) }
-}
